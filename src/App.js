d3c35e2db0c9efee6f42d074066d879a0c4c1c05
Victor Gama <vktrgama@gmail.com>
2021-02-16 15:42:50 -0800

    add react router and hooks for state mgmt

--- src/App.js
+++ src/App.js
@@ -1,13 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Dashboard from './Components/Dashboard'
 
 function App() {
+  const user = 'Victor';
+  
+  return (
+    <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+      </ul>
+
+      <hr />
+
+      {/*
+        A <Switch> looks through all its children <Route>
+        elements and renders the first one whose path
+        matches the current URL. Use a <Switch> any time
+        you have multiple routes, but you want only one
+        of them to render at a time
+      */}
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/dashboard">
+          <Dashboard user={user} />
+        </Route>
+      </Switch>
+    </div>
+  </Router>
+  );
+}
+
+// You can think of these components as "pages"
+// in your app.
+
+function Home() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          hello world!
         </p>
         <a
           className="App-link"
@@ -21,5 +75,21 @@ function App() {
     </div>
   );
 }
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+// function Dashboard() {
+//   return (
+//     <div>
+//       <h2>Dashboard</h2>
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ src/Components/Dashboard/index.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+
+//USe Hooks to load change state
+
+function Dashboard(props) {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  });
+  
+  const handleClick = () => {
+    console.log(`Click happened ${count} times`);
+    setCount(count + 1);
+  }
+
+  return (
+  <div>
+    <button onClick={handleClick}>{`Click Me ${props.user}`}</button>
+    <p>You clicked {count} times</p>
+  </div>
+  )
+}
+
+export default Dashboard;
