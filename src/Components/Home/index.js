9f3b16cb0d1a64579d57f1b1000b82b4e7b3ceaf
Victor Gama <vktrgama@gmail.com>
2021-05-03 13:59:45 -0700

    update recommendations

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -16,14 +16,14 @@ function Home() {
                         </div>
                     </div>
                 </div>
-                <div class="container">
+                <div class="container container well__ins2">
                     <div class="row">
                         <div class="grid_3 wow fadeInUp" data-wow-delay=".2s">
                             <div class="box center">
                                 <div class="tm-spanner"></div>
                                 <h4><a href="#">Development</a></h4>
                                 <p>
-                                Build and deploy engaging applications for Ancestry customers exploring their Ethnicity and Health via DNA analysis, as well as Historical records, implementing the latest full stack frameworks available, including: NodeJs, React, Redux, Jest, Mocha, WebPack, Jenkins, Docker, Kubernetes, AWS, and GitHub.
+                                Build and deliver engaging applications for Ancestry customers to explore their Ethnicity and Health via DNA analysis, as well as Historical records, implementing full stack frameworks, including: NodeJS, React, Redux, Jest, Mocha, Webpack, and deployment platforms like Jenkins, Docker, Kubernetes, AWS, and GitHub.
                                 </p>
                             </div>
                         </div>
@@ -32,7 +32,7 @@ function Home() {
                                 <div class="tm-gear"></div>
                                 <h4><a href="#">Services</a></h4>
                                 <p>
-                                Build and deploy secure backend RESTful services in Spring Boot.
+                                Build backend RESTful services for our solutions and internal clients, well documented. Support my team on several infrastructure migrations (GitHub, Kubernetes, Cloud, Harness).
                                 </p>
                             </div>
                         </div>
@@ -42,6 +42,7 @@ function Home() {
                                 <h4><a href="#">Agile</a></h4>
                                 <p>
                                 As a Scrum Master I worked with other teams to ensure dependencies are in sync, and worked with Product Managers to define a clear scope of priorities, lead my team to deliver features under tight deadlines.
+                                Close collaboration with other teams implementing internal services as well as integration with our dependencies.
                                 </p>
                             </div>
                         </div>
@@ -50,7 +51,7 @@ function Home() {
                                 <div class="tm-check"></div>
                                 <h4><a href="#">Production</a></h4>
                                 <p>
-                                Delivered 3 major products in the last 2 years for Ancestry DNA and Ancestry Health domains.
+                                Delivered 3 major products in the last 2 years for Ancestry DNA and Ancestry Health domains, high profile customer facing applications.
                                 </p>
                             </div>
                         </div>
@@ -65,32 +66,32 @@ function Home() {
                             </div>
                             <div class="grid_12">
                                 <div class="img-wrap center">
-                                    <div class="img-cnt wow bounceIn mg-add" data-wow-delay=".4s"><img src="images/index_img02.jpg" alt="" /></div>
-                                    <div class="img-cnt wow bounceIn mg-add" data-wow-delay=".2s"><img src="images/index_img03.jpg" alt="" /></div>
-                                    <div class="img-cnt wow bounceIn mg-add" data-wow-delay=".6s"><img src="images/index_img04.jpg" alt="" /></div>
+                                    <div class="img-cnt wow bounceIn mg-add" data-wow-delay=".4s"><img src="https://media-exp1.licdn.com/dms/image/C5603AQG8T4f86Modlw/profile-displayphoto-shrink_100_100/0/1556501496002?e=1625702400&v=beta&t=gb3RiuOsgSh9Dq8OJd-2A0lCwMx8tZMPAlHO8v29GHo" alt="" /></div>
+                                    <div class="img-cnt wow bounceIn mg-add" data-wow-delay=".6s"><img src="https://media-exp1.licdn.com/dms/image/C5603AQE2fdFuVJ3AhQ/profile-displayphoto-shrink_100_100/0/1610813218999?e=1625702400&v=beta&t=cimfmYLw5u7_bnovwFtdadEI76jxMeKhfLo40N1Hufs" alt="" /></div>
+                                    <div class="img-cnt wow bounceIn mg-add" data-wow-delay=".2s"><img src="https://media-exp1.licdn.com/dms/image/C5603AQEPbYIoEMwEpw/profile-displayphoto-shrink_100_100/0/1607473693611?e=1625702400&v=beta&t=mhLNQMfdxkhCshJTXk0SKWepx8V40vytpOBVWVW_Z-c" alt="" /></div>
                                 </div>
                                 <div id="owl" class="owl wow fadeInUp">
                                     <div class="item">
                                         <blockquote class="quote">
-                                            <h4 class="quote_aside"><a href="#">Robert Adams</a></h4>
+                                            <h4 class="quote_aside"><a href="#">Craig Tenney</a></h4>
                                             <p class="quote_cnt">
-                                                “Mulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptaum zzelenit augue duis dolore te feugait nulla facilisi. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem vestigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus”
+                                                “Victor is a strong software developer and I really enjoyed having him on my team. Victor contributed significantly to the release of the Ancestry Health product through his leadership as both the Technical Lead and as the Scrum Master. I relied heavily on his React and Java expertise and appreciated his ability to mentor more junior engineers. Victor has a calm demeanor and steady temperament that helped to calm the team when schedules were tight and stress levels were high. I highly recommend Victor as a very capable software engineer and all-around great guy.”
                                             </p>
                                         </blockquote>
                                     </div>
                                     <div class="item">
                                         <blockquote class="quote">
-                                            <h4 class="quote_aside"><a href="#">Monica Perry</a></h4>
+                                            <h4 class="quote_aside"><a href="#">Audrey Vincent</a></h4>
                                             <p class="quote_cnt">
-                                                “Veugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptaum zzelenit augue duis dolore te feugait nulla facilisi. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem vestigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus”
+                                                “Over the past year, Victor and I worked closely developing and launching AncestryHealth, a B2C genomics product generating over 300K DNA kit sales. Despite incredibly tight deadlines and limited resources, Victor was able to step up and lead all 6 members of the team as the interim engineering manager. Victor supported the team through crucial moments of the development process working hard to highlight and represent the success of the team. Victor's leadership and technical background proved to be essential to the success of AncestryHealth.”
                                             </p>
                                         </blockquote>
                                     </div>
                                     <div class="item">
                                         <blockquote class="quote">
-                                            <h4 class="quote_aside"><a href="#">Johm Smith</a></h4>
+                                            <h4 class="quote_aside"><a href="#">Robert Schultz</a></h4>
                                             <p class="quote_cnt">
-                                                “Aeat vero eros et accumsan et iusto odio dignissim qui blandit praesent luptaum zzelenit augue duis dolore te feugait nulla facilisi. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem vestigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus”
+                                                “Victor was a pleasure to work with, a solid engineer with a great sense of humor. He was very engaging and worked on teams that delivered high quality products in a very rapid pace. Victor was also excellent at understanding build management and owning the continuous delivery process early on in the company when we were moving to a bigger, large scale system which required lots of patience and knowledge of languages such as Ruby.”
                                             </p>
                                         </blockquote>
                                     </div>
