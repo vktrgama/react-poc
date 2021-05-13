8e1eda408e0ecf823663076d5fbbf14ab5962f38
Victor Gama <vktrgama@gmail.com>
2021-05-12 19:04:20 -0700

    fix congten conflict

--- src/Components/NetflixSlider/Content.js
+++ src/Components/NetflixSlider/Content.js
@@ -6,22 +6,22 @@ import './Content.scss';
 const IMAGE_URI = "https://www.themoviedb.org/t/p/w220_and_h330_face/";
 
 const Content = ({ movie, onClose }) => (
-  <div className="content">
-    <div className="content__background">
-      <div className="content__background__shadow" />
+  <div className="content_nf">
+    <div className="content_nf__background">
+      <div className="content_nf__background__shadow" />
       <div
-        className="content__background__image"
+        className="content_nf__background__image"
         style={{ 'background-image': `url(${IMAGE_URI + movie.poster_path})` }}
       />
     </div>
-    <div className="content__area">
-      <div className="content__area__container">
-        <div className="content__title">{movie.original_title}</div>
-        <div className="content__description">
+    <div className="content_nf__area">
+      <div className="content_nf__area__container">
+        <div className="content_nf__title">{movie.original_title}</div>
+        <div className="content_nf__description">
           {movie.overview}
         </div>
       </div>
-      <button className="content__close" onClick={onClose}>
+      <button className="content_nf__close" onClick={onClose}>
         <IconCross />
       </button>
     </div>
