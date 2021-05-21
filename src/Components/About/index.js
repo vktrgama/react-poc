85d99397b068720c04e5d6f6ecea32bdbf04d723
Victor Gama <vktrgama@gmail.com>
2021-05-21 16:52:49 -0700

    update experience

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -7,7 +7,7 @@ function About() {
             <div class="row">
                 <div class="grid_12">
                     <h2 class="wow fadeInLeft">About Me</h2>
-                    <h3 class="hdng wow fadeInRight">I have an extensive experience in the development of diverse software solutions for a variety of architectures and platforms, well familiar with Full-Stack frameworks and Object Oriented Programming over a variety of development methodologies, like Agile and Scrum.</h3>
+                    <h3 class="hdng wow fadeInRight">I have an extensive experience in the development of diverse software solutions for a variety of architectures and platforms, well familiar with Full-Stack frameworks and Object Oriented Programming over a variety of development methodologies, e.g. Agile, Scrum, or SDLC.</h3>
                 </div>
             </div>
             <TimeLine />
