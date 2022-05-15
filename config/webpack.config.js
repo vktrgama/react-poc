26d7b5939724d0cb89b3159e522742233859b43a
Victor Gama <vktrgama@gmail.com>
2022-05-15 15:16:46 -0700

    force all roiutes to index file webpack

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -190,7 +190,7 @@ module.exports = function (webpackEnv) {
             // initialization, it doesn't blow up the WebpackDevServer client, and
             // changing JS code would still trigger a refresh.
           ]
-        : paths.appIndexJs,
+        : path.join(__dirname, './src/index.js'), //TODO: restore to paths.appIndexJs, if needed, force routes for now
     output: {
       // The build folder.
       path: isEnvProduction ? paths.appBuild : undefined,
