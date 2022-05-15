3bf9eb359e1ebe5bf6d0ab11ff1091232028cce8
Victor Gama <vktrgama@gmail.com>
2022-05-15 15:19:50 -0700

    restore webpcak main entry route

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -190,7 +190,7 @@ module.exports = function (webpackEnv) {
             // initialization, it doesn't blow up the WebpackDevServer client, and
             // changing JS code would still trigger a refresh.
           ]
-        : path.join(__dirname, './src/index.js'), //TODO: restore to paths.appIndexJs, if needed, force routes for now
+        : paths.appIndexJs,
     output: {
       // The build folder.
       path: isEnvProduction ? paths.appBuild : undefined,
